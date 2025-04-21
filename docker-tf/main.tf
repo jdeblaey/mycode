c1e3515d1fa5aa5fa3981ee11e8398ffbcc5f5dc
jdeblaey <jim.deblaey@outlook.com>
2025-04-21 19:50:32 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,30 @@
+#Jim Deblaey's code
+//wonder what is next
+/* Terraform is awesome
+just freaking awesome */
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+
