e7183a13ba0734a36bf4b0a434907f8c49415105
jdeblaey <jim.deblaey@outlook.com>
2025-04-22 15:07:28 +0000

    workspaces

---
+++ docker-challenge/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+provider "docker" {}
+resource "docker_image" "simplegoservice" {
+  name         = "registry.gitlab.com/alta3/simplegoservice"
+  keep_locally = true       // keep image after "destroy"
+}
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = var.container_name
+  ports {
+    internal = 80
+    external = 8089
+  }
+}
